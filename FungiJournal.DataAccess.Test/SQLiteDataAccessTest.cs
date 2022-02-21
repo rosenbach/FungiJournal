67266cbc3e7465fd156428c80c5ae02c205b10c7
rosenbach <m_kaesdorf@hotmail.de>
2022-02-21 20:38:36 +0000

    basic test structure

---
+++ FungiJournal.DataAccess.Test/SQLiteDataAccessTest.cs
@@ -0,0 +1,18 @@
+using Xunit;
+using FluentAssertions;
+
+namespace FungiJournal.DataAccess.Test
+{
+    public class SQLiteDataAccessTest
+    {
+        [Fact]
+        public void TestIfEntryWasAdded()
+        {
+            //arrange
+
+            //act
+
+            //assert
+        }
+    }
+}
