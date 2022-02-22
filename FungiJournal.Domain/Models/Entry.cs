cb913b6ef616e0872aeb1c4210127091504ab708
rosenbach <m_kaesdorf@hotmail.de>
2022-02-22 10:57:15 +0000

    Add CodeFirstAccess.cs as a new dataAccess class
    Add DataAnnotations in the Models

--- FungiJournal.Domain/Models/Entry.cs
+++ FungiJournal.Domain/Models/Entry.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace FungiJournal.Domain.Models
 {
     public class Entry
     {
+        [Key] [Required]
         public int EntryId { get; set; }
         public DateTime? Timestamp { get; set; }
         public int? FungiId { get; set; }
