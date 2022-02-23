b44805bf701fcc753f943a71d70ea9f9cd0bce96
rosenbach <m_kaesdorf@hotmail.de>
2022-02-23 11:52:00 +0000

    Add Project FungiJournal.Api.Test
    Add Test EntriesControllerTest

---
+++ Tests/FungiJournal.Api.Test/EntriesControllerTest.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+using FluentAssertions;
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+
+namespace FungiJournal.Api.Test
+{
+    public class EntriesControllerTest
+    {
+        [Fact]
+        public void TestIfPostWasSuccessful()
+        {
+            //arrange
+
+            //act
+
+            //assert
+        }
+    }
+}
