bde986ef963ee8a4b583404dad9cc279f489291a
rosenbach <m_kaesdorf@hotmail.de>
2022-02-23 12:18:54 +0000

    Test TestIfPostWasSuccessful

--- Tests/FungiJournal.Api.Test/EntriesControllerTest.cs
+++ Tests/FungiJournal.Api.Test/EntriesControllerTest.cs
@@ -2,6 +2,10 @@
 using FluentAssertions;
 using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore;
+using FungiJournal.API.Controllers;
+using FungiJournal.DataAccess;
+using FungiJournal.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FungiJournal.Api.Test
 {
@@ -11,10 +15,21 @@ namespace FungiJournal.Api.Test
         public void TestIfPostWasSuccessful()
         {
             //arrange
+            //open the connection to the cnn
+            var options = Options.Create(new DataAccessConfiguration { ConnectionString = "TestDB" });
+            var dbContextOptions = new DbContextOptionsBuilder<CodeFirstDbContext>()
+                .UseInMemoryDatabase("123").Options;
+            var dbContext = new CodeFirstDbContext(dbContextOptions, options);
+            var sqLiteDataAccess = new SQLiteDataAccess(dbContext);
+            var sut = new EntriesController(sqLiteDataAccess);
+
+            Entry mockEntry = new Entry { EntryId = 1, Description = "Hello" };
 
             //act
+            var result = sut.PostEntry(mockEntry);
 
             //assert
+            result.Should().BeEquivalentTo(new ActionResult<Entry>(new OkObjectResult(mockEntry)));
         }
     }
 }
