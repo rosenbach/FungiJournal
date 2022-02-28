761359952d1a78e6bce68835815493c42a0127be
rosenbach <m_kaesdorf@hotmail.de>
2022-02-28 14:30:12 +0000

    Add tests for delete
    Tests run fine when executed individually.
    Needs async

--- FungiJournal.DataAccess.Test/SQLiteDataAccessTest.cs
+++ FungiJournal.DataAccess.Test/SQLiteDataAccessTest.cs
@@ -3,24 +3,17 @@ using FluentAssertions;
 using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore;
 using FungiJournal.Domain.Models;
+using System;
 
 namespace FungiJournal.DataAccess.Test
 {
     public class SQLiteDataAccessTest
     {
-        //create SQLite inMemory database
-
         [Fact]
         public void TestIfEntryWasAdded()
         {
             //arrange
-            //open the connection to the cnn
-            var options = Options.Create(new DataAccessConfiguration { UseInMemoryDatabase = true });
-
-            var dbContextOptions = new DbContextOptionsBuilder<CodeFirstDbContext>().Options;
-
-            var dbContext = new CodeFirstDbContext(dbContextOptions, options);
-            var sut = new SQLiteDataAccess(dbContext);
+            var sut = new SQLiteDataAccess(SetupTestDBContext());
 
             Entry mockEntry = CreateMockEntry();
 
@@ -34,24 +27,51 @@ namespace FungiJournal.DataAccess.Test
                 options => options.Excluding(x => x.EntryId));
         }
 
+
+        [Fact]
+        public void TestIfEntryWasDeleted()
+        {
+            //arrange
+            var sut = new SQLiteDataAccess(SetupTestDBContext());
+
+            Entry mockEntry = CreateMockEntry();
+            sut.AddEntry(mockEntry);
+            int entriesCount = sut.LoadEntries().Count;
+
+            //act
+            sut.DeleteEntry(mockEntry.EntryId);
+            var result = sut.LoadEntries().Count;
+
+            //assert
+            result.Should().Be(entriesCount - 1);
+            sut.Dispose();
+        }
+
         [Fact]
         public void TestIfDatabaseIsInMemory()
         {
             //arrange
-            var options = Options.Create(new DataAccessConfiguration { UseInMemoryDatabase = true });
-            var dbContextOptions = new DbContextOptionsBuilder<CodeFirstDbContext>().Options;
+            var sut = SetupTestDBContext();
 
             //act
-            var sut = new CodeFirstDbContext(dbContextOptions, options);
             var result = sut.Database.IsInMemory();
 
             //assert
             result.Should().Be(true);
         }
 
+
         private Entry CreateMockEntry()
         {
-            return new Entry { EntryId = 1, Description = "Mock Entry" };
+            Random rnd = new Random();
+            return new Entry { EntryId = rnd.Next(), Description = "Mock Entry" };
+        }
+
+        private CodeFirstDbContext SetupTestDBContext()
+        {
+            var options = Options.Create(new DataAccessConfiguration { UseInMemoryDatabase = true });
+            var dbContextOptions = new DbContextOptionsBuilder<CodeFirstDbContext>().Options;
+            return new CodeFirstDbContext(dbContextOptions, options);
         }
     }
 }
