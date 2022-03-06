3c7b32b34411e7bbafbe39fa0961453a9a9e8086
rosenbach <m_kaesdorf@hotmail.de>
2022-03-06 17:35:47 +0000

    added TestIfEntryWasDeleted (test successful) added TestIfEntryWasPut (test successful)

--- Tests/FungiJournal.Api.Test/EntriesControllerTest.cs
+++ Tests/FungiJournal.Api.Test/EntriesControllerTest.cs
@@ -15,7 +15,70 @@ namespace FungiJournal.Api.Test
     public class EntriesControllerTest
     {
         [Fact]
-        public async Task TestIfPostWasSuccessful()
+        public async Task TestIfGetByIdWasSuccessful()
+        {
+
+        }
+
+        [Fact]
+        public async Task TestIfGetAllWasSuccessful()
+        {
+
+        }
+
+        [Fact]
+        public async Task TestIfEntryWasDeleted()
+        {
+            //arrange
+            var sqLiteDataAccess = new SQLiteDataAccess(DataAccessMock.CreateMockDBContext());
+            var sut = new EntriesController(sqLiteDataAccess);
+            Entry mockEntry = DataAccessMock.CreateMockEntry();
+            Entry mockEntryToBeDeleted = DataAccessMock.CreateMockEntry();
+            await sut.PostEntry(mockEntry);
+            await sut.PostEntry(mockEntryToBeDeleted);
+
+            //act
+            var result = await sut.DeleteEntry(mockEntryToBeDeleted.EntryId);
+
+            //assert
+            result.Should().BeOfType<OkObjectResult>();
+            var entries = await sqLiteDataAccess.GetEntriesAsync();
+            entries.Should().ContainSingle();
+            var resultFromDatabase = entries.Single();
+            resultFromDatabase?.EntryId.Should().Be(mockEntry.EntryId);
+            resultFromDatabase?.EntryId.Should().NotBe(mockEntryToBeDeleted.EntryId);
+
+            var typedResult = result as OkObjectResult;
+            var resultingEntry = typedResult?.Value as Entry;
+            resultingEntry?.EntryId.Should().Be(mockEntryToBeDeleted.EntryId);
+            resultingEntry?.EntryId.Should().NotBe(mockEntry.EntryId);
+        }
+
+        [Fact]
+        public async Task TestIfEntryWasPut()
+        {
+            //arrange
+            var sqLiteDataAccess = new SQLiteDataAccess(DataAccessMock.CreateMockDBContext());
+            var sut = new EntriesController(sqLiteDataAccess);
+            Entry mockEntry = DataAccessMock.CreateMockEntry();
+            await sut.PostEntry(mockEntry);
+            string modifiedDescription = "I was modified";
+            mockEntry.Description = modifiedDescription;
+
+
+            //act
+            var result = await sut.PutEntry(mockEntry.EntryId, mockEntry);
+
+            //assert
+            result.Should().BeOfType<NoContentResult>();
+            var entries = await sqLiteDataAccess.GetEntriesAsync();
+            entries.Should().ContainSingle();
+            var resultFromDatabase = entries.Single();
+            resultFromDatabase.Description.Should().Be(modifiedDescription);
+        }
+
+        [Fact]
+        public async Task TestIfEntryWasPosted()
         {
             //arrange
             var sqLiteDataAccess = new SQLiteDataAccess(DataAccessMock.CreateMockDBContext());
