5ff5c8975f394f77d6b8bd669a170c0239c6bfe4
rosenbach <m_kaesdorf@hotmail.de>
2022-07-03 17:07:10 +0100

    removed obsolete method

--- FungiJournal.API/APIClients/BayernAtlasAPIClient.cs
+++ FungiJournal.API/APIClients/BayernAtlasAPIClient.cs
@@ -5,16 +5,6 @@ namespace FungiJournal.API.APIClients
 {
     public class BayernAtlasAPIClient
     {
-        public Uri GetFeatureInfo(double lat, double lng)
-        {
-            WMSRequest request = new()
-            {
-                BBox = $"{lng},{lat},{lng-0.0001},{lat-0.0001}"
-            };
-            
-            return request.Uri;
-        }
-
         public async Task<bool> IsNatureReservoir(double lat, double lng)
         {
             WMSRequest request = new()
